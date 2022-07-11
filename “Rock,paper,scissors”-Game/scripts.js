b840d25f7a11496e6a60d8f6731dc802399dd8b7
MafteiMugurel <mugurel.maftei95@gmail.com>
2022-07-11 21:12:21 +0300

    added 3 pages in 1

--- “Rock,paper,scissors”-Game/scripts.js
+++ “Rock,paper,scissors”-Game/scripts.js
@@ -33,3 +33,16 @@ function play() {
 }
 
 play();
+
+function show(elementID) {
+  var ele = document.getElementById(elementID);
+  if (!ele) {
+    alert("No Such Page Found.");
+    return;
+  }
+  var pages = document.getElementsByClassName("page");
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].style.display = "none";
+  }
+  ele.style.display = "block";
+}
