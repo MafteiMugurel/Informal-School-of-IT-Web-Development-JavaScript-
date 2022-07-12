8e894967071000397ad494daea1275f4669a8284
MafteiMugurel <mugurel.maftei95@gmail.com>
2022-07-12 18:10:16 +0300

    finished all function in js

--- Rock-Paper-Scissors-Game/scripts.js
+++ Rock-Paper-Scissors-Game/scripts.js
@@ -5,44 +5,50 @@ var compluterScore = 0;
 var userScore = 0;
 function play() {
   let computerOption = options[Math.floor(Math.random(options) * 3)];
- rounds = rounds +1;
- console.log(rounds);
+  rounds = rounds + 1;
 
-  console.log("Computer choice:" + computerOption);
-  console.log("User choice:" + userOption);
-
+  var computerOptionElement = document.getElementById("computerChoose");
+  computerOptionElement.innerText = computerOption;
   if (computerOption === userOption) {
-    console.log("Tie");
+    var roundResult = document.getElementById("winner");
+    roundResult.innerText = "Tie";
   } else {
     if (computerOption === "rock") {
       if (userOption === "scissors") {
-        compluterScore = compluterScore +1;
-        console.log("Computer Wins");
+        compluterScore = compluterScore + 1;
+        var roundResult = document.getElementById("winner");
+        roundResult.innerText = "Computer Wins";
       } else if (userOption === "paper") {
-        userScore = userScore +1;
-        console.log("User Wins");
+        userScore = userScore + 1;
+        var roundResult = document.getElementById("winner");
+        roundResult.innerText = "User Wins";
       }
     } else if (computerOption === "paper") {
       if (userOption === "scissors") {
-        userScore = userScore +1;
-        console.log("User Wins");
+        userScore = userScore + 1;
+        var roundResult = document.getElementById("winner");
+        roundResult.innerText = "User Wins";
       } else if (userOption === "rock") {
-        compluterScore = compluterScore +1;
-        console.log("Computer Wins");
+        compluterScore = compluterScore + 1;
+        var roundResult = document.getElementById("winner");
+        roundResult.innerText = "Computer Wins";
       }
     } else if (computerOption === "scissors") {
       if (userOption === "rock") {
-        userScore = userScore +1;
-        console.log("User Wins");
+        userScore = userScore + 1;
+        var roundResult = document.getElementById("winner");
+        roundResult.innerText = "User Wins";
       } else if (userOption === "paper") {
-        compluterScore = compluterScore +1;
-        console.log("Computer Wins");
+        compluterScore = compluterScore + 1;
+        var roundResult = document.getElementById("winner");
+        roundResult.innerText = "Computer Wins";
       }
     }
   }
-  console.log("User Wins" , userScore);
-  console.log("Computer Wins" ,compluterScore);
-  
+  var userWins = document.getElementById("userScore");
+  userWins.innerText = userScore;
+  var computerWins = document.getElementById("computerScore");
+  computerWins.innerText = compluterScore;
 }
 
 function show(elementID) {
@@ -60,5 +66,7 @@ function show(elementID) {
 
 function select(option) {
   userOption = option;
-  console.log(userOption);
+
+  var userSelectedElement = document.getElementById("userChoose");
+  userSelectedElement.innerText = userOption;
 }
