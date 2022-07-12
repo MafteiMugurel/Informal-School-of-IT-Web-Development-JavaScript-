6720fdf7c8b3bd0668f41ce0200356868d071b0e
MafteiMugurel <mugurel.maftei95@gmail.com>
2022-07-12 16:35:04 +0300

    score game

--- Rock-Paper-Scissors-Game/scripts.js
+++ Rock-Paper-Scissors-Game/scripts.js
@@ -1,8 +1,12 @@
 const options = ["rock", "paper", "scissors"];
-
+var userOption;
+var rounds = 0;
+var compluterScore = 0;
+var userScore = 0;
 function play() {
   let computerOption = options[Math.floor(Math.random(options) * 3)];
-  let userOption = options[Math.floor(Math.random(options) * 3)];
+ rounds = rounds +1;
+ console.log(rounds);
 
   console.log("Computer choice:" + computerOption);
   console.log("User choice:" + userOption);
@@ -12,27 +16,34 @@ function play() {
   } else {
     if (computerOption === "rock") {
       if (userOption === "scissors") {
+        compluterScore = compluterScore +1;
         console.log("Computer Wins");
       } else if (userOption === "paper") {
+        userScore = userScore +1;
         console.log("User Wins");
       }
     } else if (computerOption === "paper") {
       if (userOption === "scissors") {
+        userScore = userScore +1;
         console.log("User Wins");
       } else if (userOption === "rock") {
+        compluterScore = compluterScore +1;
         console.log("Computer Wins");
       }
     } else if (computerOption === "scissors") {
       if (userOption === "rock") {
+        userScore = userScore +1;
         console.log("User Wins");
       } else if (userOption === "paper") {
+        compluterScore = compluterScore +1;
         console.log("Computer Wins");
       }
     }
   }
+  console.log("User Wins" , userScore);
+  console.log("Computer Wins" ,compluterScore);
+  
 }
-
-play();
 
 function show(elementID) {
   var ele = document.getElementById(elementID);
@@ -45,4 +56,9 @@ function show(elementID) {
     pages[i].style.display = "none";
   }
   ele.style.display = "block";
+}
+
+function select(option) {
+  userOption = option;
+  console.log(userOption);
 }
